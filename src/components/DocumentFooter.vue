17a7f428be80c0074de8b498479ff25d3a0bd1d2
Aquarian Caroline <caroline@aquarianwebdesign.com>
2024-02-27 18:33:32 -0700

    add new footer component

---
+++ src/components/DocumentFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class='flex  bg-blue-800  '>
+		<div class="w-full  font-body px-7 ">
+            <p class="font-body text-center text-white m-12 text-large">Thanks for taking the time to check us out!</p>
+		<p class='font-body  text-center text-white m-12'> Code by Caroline C. Blaker for <a href="https://www.aquarianwebdesign.com" class='text-blue-200' title="web development agency">Aquarian Web Studio</a> using Tailwind CSS and Webpack. UI Kit Design by <a href="https://block81.com" class='text-blue-200'>Block 81</a>.</p>
+		</div>
+    </footer>
+</template>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script>
+</script>
+<style scoped>
+</style>
